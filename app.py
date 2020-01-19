740ac597c29e7f7e76b80e83071e72bb7fba4805
3ri5 <45490952+3ri5@users.noreply.github.com>
2020-01-18 18:35:32 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -16,143 +16,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'sqlite:///tes
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
-class User(db.Model):
+# Data table
+class needsmap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    pwd = db.Column(db.String(80), nullable=False)
-    favorites = db.relationship('Favorite', backref='user', lazy=True)
+    password = db.Column(db.String(80), nullable=False)
+    longitude = db.Column(db.Float(40))
+    latitude = db.Column(db.Float(40))
+    name = db.Column(db.String(30))
+    food = db.Column(db.String(20))
+    clothing = db.Column(db.String(20))
+    femprod = db.Column(db.String(20))
+    toilet = db.Column(db.String(20))
 
-
-class Favorite(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    picture_url = db.Column(db.Text)
-    quote = db.Column(db.Text)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-
-def get_cat_picture():
-    # Random cat pictures
-    url = 'https://api.thecatapi.com/v1/images/search?limit=1&order=Random&mime_types=jpg'
-    cat_api_response = requests.get(url).json()
-    return cat_api_response[0]['url']
-
-
-def get_quote():
-    # Random math quotes
-    url = 'https://random-math-quote-api.herokuapp.com/'
-    quote_api_response = requests.get(url).json()
-    return f'{quote_api_response["quote"]} - {quote_api_response["author"]}'
-
-
-@app.route('/')
-def index():
-    return render_template('index.html', url=get_cat_picture(), quote=get_quote())
-
-@app.route('/form',methods=['GET','POST'])
-def save_form():
-    if request.method == 'POST':
-        # name= request.form.get('name')
-        # if name == None:
-        #     flash('Name field is empty! Please provide a valid shelter name')
-        # else:
-        #     #check if shelter manager has access to this shelter 
-        # location = request.form.get('location')
-        # if location == None:
-        #     flash('Location field is empty! Please provide a valid shelter location') 
-        # else:
-        #     #check if shelter manager has access to this location  
-        
-        resource=request.form.get('resource')
-        if(resource == "--None--" )
-           flash('Please select a valid resource!')
-           return redirect(url_for('form'))
-        alert_number =request.form.get('alert')
-        if(alert_number == "--None--" )
-           flash('Please select a valid priority number!')
-           return redirect(url_for('form'))
-        #update db here - can the db updation be synced and recorded????
-        flash("You have successfully submitted the details! Please logout if you don't have any more updates or you can continue updating the form")
-        return redirect(url_for('form'))
-
-    else:
-        def dropdown():
-            #fetch name from db and store it in name
-
-            #fetch location from db and store it in location
-
-            resources = ['--None--','Food', 'Toiletries', 'Clothes', 'Shoes','Feminine Supplies','Monetary Donations']
-            alerts = ['--None--','0','1','2','3','4']
-            return render_template('form.html', name = name,location = location,resources=resources,alerts = alerts)
-        
-
-@app.route('/save-favorite', methods=['POST'])
-def save_favorite():
-    user = User.query.filter_by(username=session.get('username')).first()
-    url = request.form.get('url')
-    quote = request.form.get('quote')
-    if user and url and quote:
-        favorite = Favorite(picture_url=url, quote=quote, user_id=user.id)
-        db.session.add(favorite)
-        db.session.commit()
-    return redirect('/')
-
-
-@app.route('/favorites')
-def get_favorites():
-    favorites = []
-    username = session.get('username')
-    user = User.query.filter_by(username=username).first()
-    if user:
-        favorites = user.favorites
-    return render_template('favorites.html', favorites=favorites)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        password = request.form.get('password')
-        username = request.form.get('username')
-        user = User.query.filter_by(username=username).first()
-        if user is None:
-            flash('You do not have an account. Please register one.')
-            return redirect(url_for('register'))
-        if not check_password_hash(user.password, password):
-            flash('Incorrect password, try again.')
-            return redirect(url_for('login'))
-        session['username'] = user.username
-        return redirect(url_for('index'))
-    else:
-        return render_template('login.html')
-
-
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect(url_for('index'))
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    # TODO: Add email and require confirmation before creating a new account
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        location = request.form.get('location')
-        name = request.form.get('name')
-
-        user_doesnt_exist = User.query.filter_by(username=username).first() is None
-        #location does not exist,name does not exist
-        if username and password and user_doesnt_exist:
-            user = User(username=username, pwd=generate_password_hash(password),address_id= location,name=name)
-            db.session.add(user)
-            db.session.commit()
-            return redirect(url_for('login'))
-        else:
-            flash(f'The username {username} is not available, please choose another')
-            return redirect(url_for('register'))
-    else:
-        return render_template('register.html')
 #API Key Initialization
 
 app.config['GOOGLEMAPS_KEY'] = "AIzaSyDPQ4t0bcLgc1FByDOrjDZxjQL_yoEly4I"
