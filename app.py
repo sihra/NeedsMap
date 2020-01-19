9cc365d77428a01d77913d348d65f20e729c1240
Gurdev Sihra <sihragurdev@gmail.com>
2020-01-19 04:09:14 -0800

    Update app.py

--- app.py
+++ app.py
@@ -60,10 +60,10 @@ def form():
         else:
             username1 = session.get('username')
             user = needsmap.query.filter_by(username=username1).first()
-            flash('hiiiii' + resource)
+            
             if resource == "Food":
                 user.food = int(alert_number)
-                flash(user.food)
+                
             elif resource == "Toiletries":
                 user.toilet = int(alert_number)
             elif resource == "Clothes":
@@ -72,10 +72,10 @@ def form():
                 user.shoes = int(alert_number)
             elif resource == "Feminine Supplies":
                 user.femprod = int(alert_number)
-                flash(user.femprod)
+                
             elif resource == "Monetary Donations":
                 user.cash = int(alert_number)
-                flash(user.cash)
+                
             db.session.commit()
         
         flash("You have successfully submitted the details! Please logout if you don't have any more updates or you can continue updating the form")
